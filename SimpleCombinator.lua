5c59c8c483d82615ad7e2cbcbaf2e065f08de238
hexelll <139350367+hexelll@users.noreply.github.com>
2026-02-27 14:14:57 +0100

    added SimpleCombinator.lua

---
+++ SimpleCombinator.lua
@@ -0,0 +1,40 @@
+local Color = require("Color")
+
+local SimpleCombinator = {}
+
+
+local hexTable = {
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f"
+}
+
+function SimpleCombinator:new()
+    local o = {}
+    setmetatable(o,{
+        __index=function(_,k)
+            return self[k]
+        end
+    })
+    return o
+end
+
+function SimpleCombinator:findCombination(u,v,image,palette)
+    local indexColor = image:getPx(u,v):findClosest(palette)
+    return {string.char(0),"0",hexTable[indexColor]}
+end
+
+return SimpleCombinator
