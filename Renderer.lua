e6797b80cbf10f129c6661443b5886b8c83eaf35
hexelll <139350367+hexelll@users.noreply.github.com>
2026-03-09 08:38:28 +0100

    fixed* Renderer

--- Renderer.lua
+++ Renderer.lua
@@ -60,7 +60,7 @@ function Renderer:render(image,palette)
         for j=1,self.sx do
             local v,u = (i-1)/(self.sy-1),(j-1)/(self.sx-1)
             local combinator = self:getCombinator(u,v,j,i)
-            local combination = combinator:findCombination(u,v,i,j,image,palette)
+            local combination = combinator:findCombination(u,v,j,i,image,palette)
             lines[i][1] = lines[i][1]..combination[1]
             lines[i][2] = lines[i][2]..combination[2]
             lines[i][3] = lines[i][3]..combination[3]
