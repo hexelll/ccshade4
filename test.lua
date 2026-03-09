212c182419319d272368642af8f943f5b17974be
hexelll <139350367+hexelll@users.noreply.github.com>
2026-03-09 08:10:24 +0100

    update test.lua

--- test.lua
+++ test.lua
@@ -2,33 +2,13 @@ local MediaParser = require "MediaParser"
 local SimpleCombinator = require "SimpleCombinator"
 local ImageHandler = require "ImageHandler"
 local Color = require "Color"
+local Renderer = require "Renderer"
 
-local combinator = SimpleCombinator:new()
+local mon = peripheral.find("monitor")
+local width,height = mon.getSize()
 
-while true do
-    local mon = peripheral.find("monitor")
+local screen = Renderer:new{term=mon}
 
-    local width,height = mon.getSize()
-
-    local img = ImageHandler:new(width,height):process(function(self,u,v)
-        local k = (1+math.sin(os.clock()))/2
-        return Color:new(0,u*k,v*k)
-    end)
-    local palette = img:findPalette()
-    for i=1,#palette do
-        local p = palette[i]
-        mon.setPaletteColor(2^(i-1),p[1],p[2],p[3])
-    end
-    local t = os.clock()
-    for y=1,img.sy do
-        mon.setCursorPos(1,y)
-        for x=1,img.sx do
-            local u,v = x/img.sx, y/img.sy
-            local c = combinator:findCombination(u,v,img,palette)
-            mon.blit(table.unpack(c))
-        end
-    end
-    local endt = os.clock()
-    print(endt-t)
-    sleep()
-end
+screen
+:render(MediaParser:open(arg[1]))
+.display()
