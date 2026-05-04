ec7acdf0b707365e521637a26f33807b9e9ebf6e
hexelll <139350367+hexelll@users.noreply.github.com>
2026-05-04 19:48:54 +0200

    added more parameters and started work on the comments

--- combinators/ASCIICombinator.lua
+++ combinators/ASCIICombinator.lua
@@ -1,10 +1,14 @@
 --[[
 
+    this is a combinator mean to achieve a classic retro ascii look
+    it works by using the lightness of the pixel as an index into a table of characters
 
-
-
-
-
+    ASCIICombinator: {
+        new: function,
+        onPaletteChange: function,
+        onImageChange: function,
+        findCombination: function
+    }
 
 ]]
 
@@ -12,7 +16,7 @@
 
 local Color = require "Color"
 local chars = {
-    ' ','.','+','=','%','\7','\14','\15','#','@'
+    ' ','.','*','+','=','a','@'
 }
 
 local hexTable = {
@@ -36,17 +40,62 @@ local hexTable = {
 
 local combinator = {name="ASCIIcombinator"}
 
-function combinator:new()
-    local o = {}
+--[[
+
+    this function creates a new ASCIICombinator instance, 
+    this should generaly only be done once per program
+
+    function new(
+        self: ASCIICombinator,
+        args:{
+            cacheSize: ?number,
+            chars: ?[char],
+            invert: ?boolean
+        }
+    ) -> ASCIICombinator
+
+]]
+function combinator:new(args)
+    args = args and args or {}
+    local o = {
+        cacheSize = args.cacheSize and args.cacheSize or 100,
+        chars = args.chars and args.chars or chars,
+        invert = args.invert
+    }
     setmetatable(o,{__index=self})
     return o
 end
 
-function combinator:onPaletteChange()
 
+--[[
+
+    this function is called when the palette is different from last Renderer.render call
+
+    onPaletteChange(
+        self: ASCIICombinator,
+        palette: [Color],
+        renderer: Renderer
+    ) -> void
+
+]]
+function combinator:onPaletteChange(palette,renderer)
+    self.cache = {}
 end
 
-function combinator:onImageChange()
+
+--[[
+
+    this function is called by Renderer when the image is different from last Renderer.render call
+
+    onImageChange(
+        self: ASCIICombinator,
+        image: ImageHandler,
+        palette: [Color],
+        renderer: Renderer
+    ) -> void
+
+]]
+function combinator:onImageChange(image,palette,renderer)
 
 end
 
@@ -54,11 +103,44 @@ local function round(x)
     return math.floor(x+0.5)
 end
 
+--[[
+
+    this function is used in Renderer to turn image information into actual characters displayed on the monitor
+    it returns an array of size 3 in this format : 
+    [character to display, palette index in hex format for the text color, palette index in hex format for the background color]
+
+    findCombination(
+        self: ASCIICombinator,
+        u: number, 
+        v: number, 
+        image: ImageHandler, 
+        palette: [Color]
+    ) -> [char, char, char]
+
+]]
 function combinator:findCombination(u,v,image,palette)
     local px = image:getPx(u,v)
-    px = px and px or Color:new()
+    px = px and px or Color()
+    local col = Color()
+    local hash = col:toHash(self.cacheSize)
+    local cachedCol = self.cache[hash]
+    if cacheCol then
+        return cacheCol
+    end
     local k = (px[1]+px[2]+px[3])/3
-    return {chars[1+round(k*(#chars-1))],hexTable[px:findClosest(palette)],hexTable[Color:new():findClosest(palette)]}
+    if self.invert then
+        k = 1-k
+    end
+    col[1] = math.min(px[1]*(1+k),1)
+    col[2] = math.min(px[2]*(1+k),1)
+    col[3] = math.min(px[3]*(1+k),1)
+    
+    local combination = {self.chars[1+round(k*(#chars-1))],hexTable[col:findClosest(palette)],hexTable[Color():findClosest(palette)]}
+    if self.invert then
+        combination[2],combination[3] = combination[3],combination[2]
+    end
+    self.cache[hash] = combination
+    return combination
 end
 
 return combinator
