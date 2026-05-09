11ce97f595ced018f01a64ffbb8ee0be7276f8fe
hexelll <139350367+hexelll@users.noreply.github.com>
2026-05-09 17:56:41 +0200

    created installer for combox

---
+++ installer.lua
@@ -0,0 +1,89 @@
+local combinators = {
+    "ASCIICombinator",
+    "CharCombinator",
+    "FastCharCombinator",
+    "FlowCombinator",
+    "MathCharCombinator",
+    "SquarePixelCombinator",
+    "VerboseCombinator",
+    "SimpleCombinator"
+}
+
+local files = {
+    "MediaParser",
+    "png",
+    "deflate",
+    "qoi",
+    "numberlua",
+    "Color",
+    "ImageHandler",
+    "Renderer",
+    "combox"
+}
+
+if arg[1] == "-h" then
+    print("this program installs all files for ComBox \n usage: installer <path> -t\n -t => optional, installs tests folder")
+    return
+end
+
+local function githubUrl(username,repo,path)
+    return "https://api.github.com/repos/"..username.."/"..repo.."/contents/"..path
+end
+
+local function getUnwrappedResponse(url)
+    local response = {http.get(url)}
+    if not response[1] then
+        error(response[2])
+    end
+    return response[1].readAll()
+end
+
+local function ptabs(n)
+    local str = ""
+    for i=1,n do
+        str = str.."  "
+    end
+    return str
+end
+
+local function downloadRepo(path,url)
+    local response = textutils.unserializeJSON(getUnwrappedResponse(url))
+
+    for _,p in pairs(response) do
+        if p.name == "combinators" then
+            rep = textutils.unserialiseJSON(getUnwrappedResponse(p.url))
+            for i,pt in pairs(rep) do
+                for _,pc in pairs(combinators) do
+                    if pc..".lua" == pt.name then
+                        term.write(ptabs(1))
+                        print(pt.path)
+                        local h = fs.open(path..pt.path,"w")
+                        h.write(getUnwrappedResponse(pt.download_url))
+                        h.close()
+                    end
+                end
+            end
+        elseif arg[2] == "-t" and p.name == "tests" then
+            rep = textutils.unserialiseJSON(getUnwrappedResponse(p.url))
+            for _,pt in pairs(rep) do
+                term.write(ptabs(1))
+                print(pt.path)
+                local h = fs.open(path..pt.path,"w")
+                h.write(getUnwrappedResponse(pt.download_url))
+                h.close()
+            end
+        end
+        for _,pf in pairs(files) do
+            if p.name == pf..".lua" then
+                print(p.path)
+                if p.download_url then
+                    local h = fs.open(path..p.path,"w")
+                    h.write(getUnwrappedResponse(p.download_url))
+                    h.close()
+                end
+            end
+        end
+    end  
+end
+
+downloadRepo(arg[1],githubUrl("hexelll","ComBox",""))
